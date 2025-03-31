b035bd55aa0f8a6238546846f5e4e0cb4c84afe3
Jibryll Brinkley <jibryllbrink@yahoo.com>
2025-03-31 15:38:09 -0400

    Initial Commit

---
+++ PAWS/PAWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PAWSApp.swift
+//  PAWS
+//
+//  Created by Jibryll Brinkley on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct PAWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
