d64e28d52299929bfeca48c5e6cdd439043f43c9
Jibryll Brinkley <jibryllbrink@yahoo.com>
2025-04-01 22:57:16 -0400

    Enhance UI design and functionality using sample data for improved future implementation

--- PAWS/ContentView.swift
+++ PAWS/ContentView.swift
@@ -12,11 +12,50 @@ struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @Query private var pets: [Pet]
     
+    let layout = [
+        GridItem(.flexible(minimum: 120)),
+        GridItem(.flexible(minimum: 120))
+    ]
+    
     var body: some View {
         NavigationStack {
             ScrollView {
-                EmptyView()
+                LazyVGrid(columns: layout) {
+                    GridRow {
+                        ForEach(pets) { pet in
+                            NavigationLink(destination: EmptyView()) {
+                                VStack {
+                                    if let imageData = pet.photo { // check if pet photo is saved
+                                        if let image = UIImage(data: imageData) { // check if the photo data is valid
+                                            Image(uiImage: image)
+                                        }
+                                    } else {
+                                        Image(systemName: "pawprint.circle") // Handle if no photo is saved
+                                            .resizable()
+                                            .scaledToFit()
+                                            .padding(40)
+                                            .foregroundStyle(.quaternary)
+                                    }
+                                    
+                                    Spacer()
+                                    
+                                    Text(pet.name)
+                                        .font(.title.weight(.light))
+                                        .padding(.vertical)
+                                    
+                                    Spacer()
+                                }
+                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                                .background(.ultraThinMaterial)
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .circular))
+                            }
+                            .foregroundStyle(.primary)
+                        }
+                    }
+                }
+                .padding(.horizontal)
             }
+            .navigationTitle(pets.isEmpty ? "" : "Paws")
             .overlay {
                 if pets.isEmpty {
                     CustomContentUnavailableView(
@@ -32,4 +71,9 @@ struct ContentView: View {
 #Preview("No Data") {
     ContentView()
         .modelContainer(for: Pet.self, inMemory: true)
+}
+
+#Preview("Sample Data") {
+    ContentView()
+        .modelContainer(Pet.preview)
 }
--- PAWS/Pet.swift
+++ PAWS/Pet.swift
@@ -18,3 +18,22 @@ final class Pet {
         self.photo = photo
     }
 }
+
+extension Pet {
+    @MainActor
+    static var preview: ModelContainer {
+        let configuration = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try! ModelContainer(for: Pet.self, configurations: configuration)
+        
+        container.mainContext.insert(Pet(name: "Max"))
+        container.mainContext.insert(Pet(name: "Charlie"))
+        container.mainContext.insert(Pet(name: "Rocky"))
+        container.mainContext.insert(Pet(name: "Bella"))
+        container.mainContext.insert(Pet(name: "Sadie"))
+        container.mainContext.insert(Pet(name: "Cooper"))
+        container.mainContext.insert(Pet(name: "Luna"))
+        container.mainContext.insert(Pet(name: "Daisy"))
+        
+        return container
+    }
+}
