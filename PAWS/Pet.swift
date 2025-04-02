c033b1a772f7c4d75ef2dda6f70def4bf695c953
Jibryll Brinkley <jibryllbrink@yahoo.com>
2025-04-01 20:12:18 -0400

    setup SwiftData + create empty state screen w/ reusable component (CustomContentUnavailableView)

---
+++ PAWS/Pet.swift
@@ -0,0 +1,20 @@
+//
+//  Pet.swift
+//  PAWS
+//
+//  Created by Jibryll Brinkley on 4/1/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Pet {
+    var name: String
+    @Attribute(.externalStorage) var photo: Data?
+    
+    init(name: String, photo: Data? = nil) {
+        self.name = name
+        self.photo = photo
+    }
+}
